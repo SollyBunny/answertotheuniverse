9ced3040ab111665e1ebb874e0a194b64efc0f3c
SollyBunny <sollybunn@gmail.com>
2023-05-28 09:06:45 +0100

    better c# naming

--- main.cs
+++ main.cs
@@ -1,6 +1,6 @@
 //usr/bin/csc /nologo /out:a main.cs && mono ./a && rm -f a && exit
-namespace MainNS {
-	class MainCL {         
+namespace ATTU {
+	class main {         
 		static void Main(string[] args) {
 			System.Console.WriteLine("7");
 		}
